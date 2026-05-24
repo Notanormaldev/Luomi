1c98b228574258748b199b8d93a4bf2ef1c3ab11
Notanormaldev <harshpatel20050@gmail.com>
2026-05-24 16:22:54 +0530

    express-validator

---
+++ Backend/src/validator/auth.validator.js
@@ -0,0 +1,26 @@
+import { body,validationResult } from "express-validator";
+
+
+function validatereq(req,res,next){
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    next()
+
+}
+
+export const validatonregister=[
+    body("email").isEmail().withMessage("INVALID EMAIL"),
+
+    body('contact').notEmpty().withMessage("contact required")
+    .matches('/^[0-9]{10}$/').withMessage("contact must be 10 digit number"),
+
+    body('password').isLength({min:8}).matches('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/').withMessage("password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character"),
+
+    body('fullname').notEmpty().withMessage("fullname required")
+    .isLength({min:3}).withMessage("fullname must be 3 chars long"),
+
+
+    validatereq
+]
