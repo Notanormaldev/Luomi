699ee72946118f185903cca97a80cf31851c5db2
Notanormaldev <harshpatel20050@gmail.com>
2026-05-25 19:00:54 +0530

    feat: update package dependencies and integrate CORS middleware for enhanced API security

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,11 +2,15 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
 import authrouter from './Routes/auth.route.js'
-
+import cors from 'cors'
 const app=express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("dev"))
-
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+    Methods:["GET","POST","PUT","DELETE"],
+}))
 app.use('/api/auth',authrouter)
 export default app
