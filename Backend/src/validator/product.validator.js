7b077bc763044361d815104b299b179754bd9f8b
Notanormaldev <harshpatel20050@gmail.com>
2026-05-28 14:18:52 +0530

    feat(product): add validation for product creation request

---
+++ Backend/src/validator/product.validator.js
@@ -0,0 +1,22 @@
+import { body,validationResult } from "express-validator";
+
+
+function validatereq(req,res,next){
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    next()
+
+}
+
+
+export const validatecreateproduct=[
+    body("title").notEmpty().withMessage("title is required")
+    .isLength({min:3}).withMessage("title must be 3 chars long"),
+    body("description").optional().isLength({max:500}).withMessage("description must be less than 500 chars"),
+    body("price.amount").notEmpty().withMessage("price is required")
+    .isNumeric().withMessage("price must be a number"),
+    body("price.currency").optional().isIn(['USD','INR','JPY','GBP','EUR']).withMessage("currency must be one of USD, INR, JPY, GBP, EUR"),
+    validatereq
+]
