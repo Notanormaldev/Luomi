f7aba6b62854714c51e6fc614fd14235af63adbf
Notanormaldev <harshpatel20050@gmail.com>
2026-05-28 08:23:59 +0530

    feat(product): create product model with schema definition

---
+++ Backend/src/models/product.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+
+
+const productSchema = mongoose.Schema({
+    title:{
+        type:String,
+        require:true
+    },
+    description:{
+       type:String
+    },
+    images:{
+       url:{
+        type:String,
+        require:true
+       }
+    },
+    price:{
+       amount:{
+        type:Number,
+        require:true
+       },
+       currency:{
+        type:String,
+        enum:['USD','INR','JPY','GBP','EUR'],
+        default:"INR"
+       }
+    },
+    seller:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users",
+        required:true
+    }
+},{timestamps:true})
+
+
+
+const productmodel = mongoose.model('products',productSchema)
+
+
+export default productmodel
